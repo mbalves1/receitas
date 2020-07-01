2eacf9f95f66324e6028d00889c758a227ba3e61
Murilo B Alves <61016975+mbalves1@users.noreply.github.com>
2020-07-01 15:53:30 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,25 +9,22 @@ const $campoBusca = $('.busca');
             }).show().replace("$textoCampoBusca","<span>${$textoCampoBusca}</span>");
         } else {
             $('.cartao').show();
-            
-         })
-         
-         /*   const $campoBusca1 = $('.busca');
-        
-            $campoBusca1.on('input', function () {
-                const $textoCampoBusca2 = $(this).val().trim();
-        
-                if ($textoCampoBusca2.length > 0) {
-                    $('.cartao').hide().filter(function () {
-                        return $(this).find(".cartao-conteudo").text().match(new RegExp($textoCampoBusca2, "i"));
-                    }).show().replace("$textoCampoBusca2","<span>${$textoCampoBusca2}</span>");
-                } else {
-                    $('.cartao').show();
-                }
-            })
-          
-        }*/
-   
+        }
+    })
+  
+    // const $campoBusca1 = $('.busca');
+
+    // $campoBusca1.on('input', function () {
+    //     const $textoCampoBusca2 = $(this).val().trim();
+
+    //     if ($textoCampoBusca2.length > 0) {
+    //         $('.cartao').hide().filter(function () {
+    //             return $(this).find(".cartao-conteudo").text().match(new RegExp($textoCampoBusca2, "i"));
+    //         }).show().replace("$textoCampoBusca2","<span>${$textoCampoBusca2}</span>");
+    //     } else {
+    //         $('.cartao').show();
+    //     }
+    // })
   
 // **************************
 
